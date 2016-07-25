20b9dd7d9348c4bd4a40dc8b52ce8fd91005a2b3
Katrina Lee <katrinamarielee@gmail.com>
2016-07-25 15:51:37 +1000

    made responsive

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,13 +1,13 @@
 
-var y = $(".destination_header").data("longitude");
-var x = $(".destination_header").data("latitude");
-
-var weatherURL = 'api.openweathermap.org/data/2.5/weather?lat={'+x+'}&lon={'+y+'}&key=b4fbb16c4a73a71494ce6441bd70d528'
-;
-var xhr = new XMLHttarpRequest(); // Instantiate our AJAX request object.
-xhr.open('GET', weatherURL); // Set up our request: method and the URL or path
-
-$.ajax(weatherURL).done(function(data){ // success is the callback function and equivalent to readystate=4
-    $('#destinationTemp').attr('src', data.temp);
-
-  )};
+// var y = $(".destination_header").data("longitude");
+// var x = $(".destination_header").data("latitude");
+//
+// var weatherURL = 'api.openweathermap.org/data/2.5/weather?lat={'+x+'}&lon={'+y+'}&key=b4fbb16c4a73a71494ce6441bd70d528'
+// ;
+// var xhr = new XMLHttarpRequest(); // Instantiate our AJAX request object.
+// xhr.open('GET', weatherURL); // Set up our request: method and the URL or path
+//
+// $.ajax(weatherURL).done(function(data){ // success is the callback function and equivalent to readystate=4
+//     $('#destinationTemp').attr('src', data.temp);
+//
+//   )};
